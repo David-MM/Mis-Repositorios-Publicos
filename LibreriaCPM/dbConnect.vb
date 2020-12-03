1774140e37dd6bcb9f44047c1164843e6692b300
Gabriel Hernandez <49258412+gabrielhamaya@users.noreply.github.com>
2020-12-02 21:46:12 -0600

    Clase de Conexion

---
+++ LibreriaCPM/dbConnect.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Data.SqlClient
+Public Class dbConnect
+    Private conexion As SqlConnection
+    Private conexionActiva As Boolean
+
+    Public ReadOnly Property ConexionSQLServer As SqlConnection
+        Get
+            Return conexion
+        End Get
+    End Property
+
+    Public ReadOnly Property Activa As Boolean
+        Get
+            Return conexionActiva
+        End Get
+    End Property
+
+    'Constructor vacio
+    Public Sub New()
+
+    End Sub
+
+    'Crea conexion
+    Public Sub New(usuario As String, password As String, Servidor As String, BaseDatos As String)
+        Try
+            'Dim url = "Server=" & "localhost" & "; DataBase=" & BaseDatos & "; UID=" & usuario & "; pwd=" + password
+            Dim url = "Server=" & "localhost" & "; DataBase=" & "CPM" & "; Integrated Security = True"
+            conexion = New SqlConnection(url)
+
+            conexionActiva = False
+            conexion.Open()
+            conexionActiva = True
+        Catch ex As SqlException
+            For Each Err As SqlError In ex.Errors
+                If Err.Number = 4060 Then
+                    MsgBox("La base de datos no existe", MsgBoxStyle.Critical, "Error")
+                End If
+                MsgBox(Err.Message, MsgBoxStyle.Critical, "Error -" & Err.Number)
+            Next
+        End Try
+    End Sub
+End Class
